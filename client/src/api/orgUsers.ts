da4de6329529b5b73b402f3f8b5ebadbe07d16a8
Viresh Mistry <vireshkumarmistry@gmail.com>
2025-06-30 16:38:24 +0530

    fix: fixed org user api

--- client/src/api/orgUsers.ts
+++ client/src/api/orgUsers.ts
@@ -1,7 +1,7 @@
 import api from "./axios";
 
 export const fetchOrgUsers = async (token: string) => {
-  const res = await api.get("/auth/org-users", {
+  const res = await api.get("api/auth/org-users", {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res.data;
